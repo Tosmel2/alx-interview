1d40d80332dff7633680b645a15eb75284aa3e77
“tosmel2” <adewaletosin0808@gmail.com>
2022-06-21 09:13:14 +0100

    script that prints all characters of a Star Wars movie:

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,17 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const endpoint = 'https://swapi-api.hbtn.io/api';
-const filmId = process.argv[2];
-
-request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
-  if (error) return console.log(error);
-
-  let characters = JSON.parse(body).characters;
-
-  for (const character of characters) {
-    await new Promise((resolve, reject) => {
-      request(character, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          console.log(JSON.parse(body).name);
-          resolve(body);
-        }
-      });
-    });
-  }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
